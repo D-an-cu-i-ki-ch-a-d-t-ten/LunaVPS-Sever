9b4d82cb4914ed1f1797da190c31705f0943edda
tuong1901 <hoanghoangcattuong@gmail.com>
2026-01-10 16:39:52 +0700

    Delete src directory

--- src/main/java/com/lunavps/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lunavps.repository;
-
-import com.lunavps.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
